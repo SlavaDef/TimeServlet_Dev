f5a9c1d87fa72f12a65f62a08681a65b2f3e9035
SlavaDef <slavadef@gmail.com>
2023-06-22 11:55:22 +0300

    commit

--- src/main/java/goIt/homework/com/Time_showing/TimeServlet.java
+++ src/main/java/goIt/homework/com/Time_showing/TimeServlet.java
@@ -1,25 +1,33 @@
 package goIt.homework.com.Time_showing;
 
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @WebServlet(value = "/time")
 public class TimeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern(
-                "yyyy-MM-dd HH:mm:ss "
-        ));
-        resp.getWriter().write(currentTime);
 
-        resp.getWriter().write(" UTC");
+        String timezone = req.getParameter("timezone");
+        resp.setContentType("text/html; charset=utf-8");
+
+        String date = new SimpleDateFormat(" yyyy-MM-dd HH:mm:ss ").format(new Date());
+
+        resp.getWriter().write(date);
+        resp.getWriter().write("${timezone}".replace("${timezone}", parseName(req)));
         resp.getWriter().close();
+    }
+
+    private String parseName(HttpServletRequest req) { // парс метод щоб не викидало помилку якщо не внисимо значень
+        if (req.getParameterMap().containsKey("timezone")) {
+            return req.getParameter("timezone");
+        }
+        return "UTC";
     }
 }
