f3303c713bb270ed3f9ecce2349e049bdcc8fa11
SlavaDef <slavadef@gmail.com>
2023-08-01 22:44:05 +0300

    F55 commit

--- src/main/java/goIt/homework/com/Time_showing/TimeServlet.java
+++ src/main/java/goIt/homework/com/Time_showing/TimeServlet.java
@@ -19,7 +19,7 @@ import java.util.Map;
 
 @WebServlet(value = "/time")
 public class TimeServlet extends HttpServlet {
-
+    private String date;
     private TemplateEngine engine;
 
     @Override
@@ -29,7 +29,7 @@ public class TimeServlet extends HttpServlet {
         FileTemplateResolver resolver = new FileTemplateResolver();
         resolver.setPrefix("C:\\Program Files\\Apache Software Foundation\\Tomcat 10.1\\bin\\templates/");
         // resolver.setPrefix("./homework");
-       // resolver.setPrefix(getClass().getClassLoader().getResource("templates").getPath());
+        // resolver.setPrefix(getClass().getClassLoader().getResource("templates").getPath());
         resolver.setSuffix(".html");
         resolver.setTemplateMode("HTML5");
         resolver.setOrder(engine.getTemplateResolvers().size());
@@ -40,30 +40,34 @@ public class TimeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html; charset=utf-8");
-
         Cookie[] lastTimezone = req.getCookies();
 
+        String timezone = req.getParameterMap().containsKey("timezone")
+                ? req.getParameter("timezone").replace(" ", "+")
+                : lastTimezone[0].getValue();
 
-        String timezone =
-                req.getParameterMap().containsKey("timezone")
-                        ? req.getParameter("timezone").replace(" ", "+")
-                        : lastTimezone[0].getValue();
+        if (req.getCookies() == null) {
+            date = new SimpleDateFormat(" yyyy-MM-dd HH:mm:ss 'UTC'").format(new Date());
+        }
+        if (!req.getParameterMap().containsKey("timezone") & req.getCookies() != null) {
+            date = DateTimeFormatter.ofPattern(" yyyy-MM-dd HH:mm:ss ")
+                    .format(LocalDateTime.now(ZoneId.of(timezone))) + "" + timezone;
+        }
 
-
-        resp.addCookie(new Cookie("timezone", timezone));
-
-
-        String timeDate = DateTimeFormatter.ofPattern(" yyyy-MM-dd HH:mm:ss ")
-                .format(LocalDateTime.now(ZoneId.of(timezone))) + "" + timezone;
-
+        if (req.getParameterMap().containsKey("timezone")) {
+            resp.addCookie(new Cookie("timezone", timezone));
+            date = DateTimeFormatter.ofPattern(" yyyy-MM-dd HH:mm:ss ")
+                    .format(LocalDateTime.now(ZoneId.of(timezone))) + "" + timezone;
+        }
 
         Context simpleContext = new Context(
                 req.getLocale(),
-                Map.of("params", timeDate)
+                Map.of("params", date)
         );
 
         engine.process("homework", simpleContext, resp.getWriter());
-
         resp.getWriter().close();
     }
 }
+
+
